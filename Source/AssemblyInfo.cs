fe808e81a710b90880e19ee73e88db11ef8b601c
Einar Ingebrigtsen <einar@dolittle.com>
2016-11-29 16:16:56 +0100

    Putting in place descriptions

--- Source/AssemblyInfo.cs
+++ Source/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Cratis.Specifications")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Implementation of the Specifications pattern")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Cratis.Specifications")]
