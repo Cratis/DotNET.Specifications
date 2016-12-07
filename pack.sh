169d88ddf1d7ea458a2396ea024bab71e01b780e
Einar Ingebrigtsen <einar@dolittle.com>
2016-12-07 14:43:00 +0100

    Explicit NuGet spec

--- pack.sh
+++ pack.sh
@@ -1,2 +1,15 @@
 #! /bin/bash
-dotnet pack ./Source -c Release -o artefacts --version-suffix nightly001
+rm -rf artefacts
+mkdir artefacts
+mkdir artefacts/lib
+mkdir artefacts/lib/netcoreapp1.1
+cd ./Source
+dotnet build
+cd ..
+cp ./Source/bin/Debug/netcoreapp1.1/*.dll ./artefacts/lib/netcoreapp1.1
+cp ./Source/bin/Debug/netcoreapp1.1/*.pdb ./artefacts/lib/netcoreapp1.1
+cp ./Source/Package.nuspec ./artefacts
+cd artefacts
+nuget pack Package.nuspec -symbols -version 1.0.0 -suffix nightly003 
+
+#dotnet pack ./Source -c Release -o artefacts --version-suffix nightly001
